dc061bbc935003fc024a30862d0c976a43792d20
Krzysztof Kasprzak <Krzysztof.Kasprzak@bglgroup.co.uk>
2021-09-14 18:54:02 +0100

    :lipstick: Centres canvas and defines its dimensions

---
+++ src/bieda-snake-main.js
@@ -0,0 +1,8 @@
+// canvas setup 
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = 600;
+const height = canvas.height = 530;
+
