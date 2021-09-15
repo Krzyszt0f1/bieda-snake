b35d43f69f3aaabfe2a8691dda9a55c824693614
Krzysztof Kasprzak <Krzysztof.Kasprzak@bglgroup.co.uk>
2021-09-15 09:52:36 +0100

    :lipstick: Adds a way to generate checkered pattern with canvas

--- src/bieda-snake-main.js
+++ src/bieda-snake-main.js
@@ -3,6 +3,18 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = 600;
-const height = canvas.height = 530;
+const width = canvas.width = 425;
+const height = canvas.height = 375;
 
+// canvas checkered pattern
+
+for (let i = 0; i < 15; i++) {
+    for (let j = 0; j < 17; j++) {
+       if((j % 2 === 0 && i % 2 === 0) || (j % 2 === 1 && i % 2 === 1)) {
+           ctx.fillStyle = '#aad751';
+       } else {
+           ctx.fillStyle = '#a2d149';
+       }
+        ctx.fillRect(j * 25, i * 25, 25, 25);
+    }
+}
